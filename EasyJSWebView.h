7624b5d342c8527590c75b719a6ef99e471d9d7b
Ming-Ray Hsu <haraguroicha@kfsyscc.org>
2014-06-09 18:14:32 +0800

    add ProgressDelegate

--- EasyJSWebView.h
+++ EasyJSWebView.h
@@ -9,10 +9,22 @@
 #import <UIKit/UIKit.h>
 #import "EasyJSWebViewProxyDelegate.h"
 
+@class EasyJSWebView;
+
+@protocol EasyJSWebViewProgressDelegate <NSObject>
+@optional
+- (void) webView:(EasyJSWebView*)webView didReceiveResourceNumber:(int)resourceNumber totalResources:(int)totalResources;
+@end
+
 @interface EasyJSWebView : UIWebView
 
 // All the events will pass through this proxy delegate first
 @property (nonatomic, retain) EasyJSWebViewProxyDelegate* proxyDelegate;
+
+@property (nonatomic, assign) int resourceCount;
+@property (nonatomic, assign) int resourceCompletedCount;
+
+@property (nonatomic, assign) IBOutlet id<EasyJSWebViewProgressDelegate> progressDelegate;
 
 - (void) initEasyJS;
 - (void) addJavascriptInterfaces:(NSObject*) interface WithName:(NSString*) name;
--- EasyJSWebView.m
+++ EasyJSWebView.m
@@ -8,9 +8,19 @@
 
 #import "EasyJSWebView.h"
 
+@interface UIWebView ()
+-(id)webView:(id)view identifierForInitialRequest:(id)initialRequest fromDataSource:(id)dataSource;
+-(void)webView:(id)view resource:(id)resource didFinishLoadingFromDataSource:(id)dataSource;
+-(void)webView:(id)view resource:(id)resource didFailLoadingWithError:(id)error fromDataSource:(id)dataSource;
+@end
+
 @implementation EasyJSWebView
 
 @synthesize proxyDelegate;
+
+@synthesize progressDelegate;
+@synthesize resourceCount;
+@synthesize resourceCompletedCount;
 
 - (id)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
@@ -51,6 +61,29 @@
 
 - (void) addJavascriptInterfaces:(NSObject*) interface WithName:(NSString*) name{
 	[self.proxyDelegate addJavascriptInterfaces:interface WithName:name];
+}
+
+-(id)webView:(id)view identifierForInitialRequest:(id)initialRequest fromDataSource:(id)dataSource
+{
+    [super webView:view identifierForInitialRequest:initialRequest fromDataSource:dataSource];
+    return [NSNumber numberWithInt:resourceCount++];
+}
+
+- (void)webView:(id)view resource:(id)resource didFailLoadingWithError:(id)error fromDataSource:(id)dataSource {
+    [super webView:view resource:resource didFailLoadingWithError:error fromDataSource:dataSource];
+    resourceCompletedCount++;
+    if ([self.progressDelegate respondsToSelector:@selector(webView:didReceiveResourceNumber:totalResources:)]) {
+        [self.progressDelegate webView:self didReceiveResourceNumber:resourceCompletedCount totalResources:resourceCount];
+    }
+}
+
+-(void)webView:(id)view resource:(id)resource didFinishLoadingFromDataSource:(id)dataSource
+{
+    [super webView:view resource:resource didFinishLoadingFromDataSource:dataSource];
+    resourceCompletedCount++;
+    if ([self.progressDelegate respondsToSelector:@selector(webView:didReceiveResourceNumber:totalResources:)]) {
+        [self.progressDelegate webView:self didReceiveResourceNumber:resourceCompletedCount totalResources:resourceCount];
+    }
 }
 
 @end
